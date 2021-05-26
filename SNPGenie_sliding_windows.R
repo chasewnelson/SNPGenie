565ed29ad520746b19bc0e767c3bb38124ed05ed
Chase W. Nelson <cwnelson88@gmail.com>
2021-05-26 19:23:51 +0800

    Update SNPGenie_sliding_windows.R
    A substantial update adding the ability to analyze codon results from the between-group script, several additional checks, and warnings for anticipated user input problems

--- SNPGenie_sliding_windows.R
+++ SNPGenie_sliding_windows.R
@@ -20,22 +20,22 @@ ARGV <- commandArgs(trailingOnly = T)
 kill_script <- FALSE
 
 if(! (length(ARGV) >= 5)) {
-  cat("Error 1")
+  cat("\n### Command line Error 1.\n")
   kill_script <- TRUE
 } else if(! str_detect(string = ARGV[1], pattern = "\\w")) {
-  cat("Error 2")
+  cat("\n### Command line Error 2.\n")
   kill_script <- TRUE
 } else if(! (ARGV[2] == "N" || ARGV[2] == "T")) {
-  cat("Error 3")
+  cat("\n### Command line Error 3.\n")
   kill_script <- TRUE
 } else if(! (ARGV[3] == "S" || ARGV[3] == "N")) {
-  cat("Error 4")
+  cat("\n### Command line Error 4.\n")
   kill_script <- TRUE
 } else if(! (str_detect(string = ARGV[4], pattern = "\\d") && as.integer(ARGV[4]) >= 2)) {
-  cat("Error 5")
+  cat("\n### Command line Error 5.\n")
   kill_script <- TRUE
 } else if(! (str_detect(string = ARGV[5], pattern = "\\d") && as.integer(ARGV[5]) >= 1)) {
-  cat("Error 6")
+  cat("\n### Command line Error 6.\n")
   kill_script <- TRUE
 } # could add more conditions later
 
@@ -50,7 +50,8 @@ if(kill_script) {
   cat("    (7) MINIMUM NUMBER OF DEFINED CODONS PER CODON POSITION (OPTIONAL; ≥2; DEFAULT=6)\n")
   cat("    (8) MULTIPLE HITS CORRECTION (OPTIONAL; \"NONE\" or \"JC\"; DEFAULT=NONE)\n")
   cat("    (9) NUMBER OF CPUS (OPTIONAL; ≥1; DEFAULT=1)\n")
-  cat("    (10) STRING TO PREPEND TO OUTPUT LINES (OPTIONAL; DEFAULT=\"\")\n\n")
+  cat("    (10) STRING TO PREPEND TO OUTPUT LINES (OPTIONAL; DEFAULT=\"\")\n")
+  cat("Contact author if this help is insufficient.\n\n")
   quit(save = 'no', status = 1, runLast = TRUE)
 } #else {
 #  cat("\n\n### RUNNING! ###\n")
@@ -94,11 +95,13 @@ if(! is.na(ARGV[10])) {
 
 # EXAMPLES
 #CODON_RESULTS_FILE <- "/Users/cwnelson88/Desktop/SCIENCE/SARS-CoV-2-South-Africa/intrahost_sliding_windows_size40/codon_results_byProductCodon_E_WINDOWS_dNdS.tsv"
+#CODON_RESULTS_FILE <- "/Users/cwnelson88/Desktop/SCIENCE/SNPGenie/Dan/between_group_codon_results.txt" # between-group example failure
+#CODON_RESULTS_FILE <- "/Users/cwnelson88/Desktop/SCIENCE/SNPGenie/Dan/between_group_codon_results_oneComp_oneProduct.txt" # between-group example success
 #NUMERATOR <- "N"
 #DENOMINATOR <- "S"
 #WINDOW_SIZE <- 10
 #STEP_SIZE <- 1
-#MIN_DEFINED_CODONS <- 2
+#MIN_DEFINED_CODONS <- 90 # 100 # 2
 #NBOOTSTRAPS <- 100
 #CORRECTION <- 'NONE'
 #NCPUS <- 6
@@ -126,12 +129,29 @@ if('N_diffs_vs_ref' %in% names(codon_data)) {
   cat("\n###############################################################################\n")
   cat("ALERT: bootstrapping assumes reliable estimates of N and S differences for all codons.\n")
   cat("    For snpgenie.pl results, this means there should be sufficient coverage at each site,\n")
-  cat("    and variants should be filtered for false positives. It is incumbent on the user to check these issues.\n")
+  cat("    and variants should be filtered for false positives.\nIt is incumbent on the user to check these issues.\n")
+} 
+
+# If it's a between-group output
+if('num_defined_codons_g1' %in% names(codon_data)) {
+  cat("\n###############################################################################\n")
+  cat("BETWEEN-GROUP FILE DETECTED. SNPGenie will require both groups to have at least\n")
+  cat("the user-specified number of MIN_DEFINED_CODONS:", MIN_DEFINED_CODONS, ".\n")
+  
+  # Calculate minimum number defined codons for the two groups being compared
+  min_num_defined_codons <- apply(data.frame(num_defined_codons_g1 = codon_data$num_defined_codons_g1, num_defined_codons_g2 = codon_data$num_defined_codons_g2),
+                                  1, min) # 1 = row-wise
+  codon_data$num_defined_seqs <- min_num_defined_codons
+  
+  # Reformat the codon_num values for consistency
+  codon_data$codon_num <- as.integer(str_replace(string = codon_data$codon_num, pattern = "codon_", replacement = ""))
 }
 
 # Check for unique codon set
-if(length(codon_data$codon_num) != unique(length(codon_data$codon_num))) {
-  cat("\n\n### WARNING: there must only be results for ONE GENE PRODUCT PER FILE, i.e., a unique set of codons per frame.:\n")
+if(length(unique(codon_data$product)) != 1 || length(codon_data$codon_num) != length(unique(codon_data$codon_num))) {
+  cat("\n\n### WARNING: there must be results for ONLY ONE GENE PRODUCT PER FILE, i.e., a unique set of codons per frame.\n")
+  cat("### Additionally, for between-group comparisons, there must be results for ONLY ONE GROUP-TO-GROUP COMPARISON.\n")
+  cat("### TERMINATED.\n\n")
   quit(save = 'no', status = 1, runLast = TRUE)
 }
 
@@ -358,100 +378,116 @@ cat("Performing sliding window analysis...\n\n")
 codon_data_filtered <- filter(codon_data, num_defined_seqs >= MIN_DEFINED_CODONS)
 codon_data_filtered <- dplyr::arrange(codon_data_filtered, codon_num)
 
-for(i in 1:(nrow(codon_data_filtered) - WINDOW_SIZE + 1)) { # each window starting at row 1
-  #i <- 11
+if(nrow(codon_data_filtered) >= WINDOW_SIZE) {
+  for(i in (min(codon_data_filtered$codon_num)):(max(codon_data_filtered$codon_num) - WINDOW_SIZE + 1)) {
+    #for(i in 1:(nrow(codon_data_filtered) - WINDOW_SIZE + 1)) { # each window starting at row 1
+    #i <- 1 # 111
+    #cat(i, "")
+    
+    # Extract window; analyze
+    window_codon_data <- codon_data_filtered[i:(i + WINDOW_SIZE - 1), ]
+    lowest_codon_num <- min(window_codon_data$codon_num)
+    highest_codon_num <- max(window_codon_data$codon_num)
+    
+    if(nrow(window_codon_data) > 1) {
+      
+      # WARNING if missing codon numbers
+      if(nrow(window_codon_data) != WINDOW_SIZE) {
+        cat("### WARNING: codon", i, "is missing.")
+        cat("### If this was unexpected or is common, there may be problems with your data.")
+      }
+      
+      ### BOOTSTRAP EACH RATIO FOR THIS WINDOW. Columns:
+      #[1] num_replicates
+      #[2] dN
+      #[3] dS
+      #[4] dNdS
+      #[5] dN_m_dS
+      #[6] boot_dN_SE
+      #[7] boot_dS_SE
+      #[8] boot_dN_over_dS_SE
+      #[9] boot_dN_over_dS_P
+      #[10] boot_dN_m_dS_SE
+      #[11] boot_dN_m_dS_P
+      #[12] boot_dN_gt_dS_count
+      #[13] boot_dN_eq_dS_count
+      #[14] boot_dN_lt_dS_count
+      #[15] ASL_dN_gt_dS_P
+      #[16] ASL_dN_lt_dS_P
+      
+      # dN/dS bootstrap with appropriate correction
+      boot_dNdS <- NA
+      if(CORRECTION == "JC") {
+        boot_dNdS <- dNdS_diff_boot_fun_JC(window_codon_data, NUMERATOR, DENOMINATOR, NBOOTSTRAPS, NCPUS)
+      } else {
+        boot_dNdS <- dNdS_diff_boot_fun(window_codon_data, NUMERATOR, DENOMINATOR, NBOOTSTRAPS, NCPUS)
+      }
+      boot_dNdS <- str_split(string = boot_dNdS, pattern = '\t')[[1]]
+      
+      # bootstrap results
+      num_replicates <- as.integer(boot_dNdS[1])
+      dN <- as.numeric(boot_dNdS[2])
+      dS <- as.numeric(boot_dNdS[3])
+      dNdS <- as.numeric(boot_dNdS[4])
+      dN_m_dS <- as.numeric(boot_dNdS[5])
+      suppressWarnings(boot_dN_SE <- as.numeric(boot_dNdS[6]))
+      suppressWarnings(boot_dS_SE <- as.numeric(boot_dNdS[7]))
+      suppressWarnings(boot_dN_over_dS_SE <- as.numeric(boot_dNdS[8]))
+      suppressWarnings(boot_dN_over_dS_P <- as.numeric(boot_dNdS[9]))
+      suppressWarnings(boot_dN_m_dS_SE <- as.numeric(boot_dNdS[10]))
+      suppressWarnings(boot_dN_m_dS_P <- as.numeric(boot_dNdS[11]))
+      suppressWarnings(boot_dN_gt_dS_count <- as.integer(boot_dNdS[12]))
+      suppressWarnings(boot_dN_eq_dS_count <- as.integer(boot_dNdS[13]))
+      suppressWarnings(boot_dN_lt_dS_count <- as.integer(boot_dNdS[14]))
+      suppressWarnings(ASL_dN_gt_dS_P <- as.numeric(boot_dNdS[15]))
+      suppressWarnings(ASL_dN_lt_dS_P <- as.numeric(boot_dNdS[16]))
+      ASL_dNdS_P <- 1
+      
+      if(! is.na(ASL_dN_gt_dS_P) && ! is.na(ASL_dN_lt_dS_P) && ASL_dN_gt_dS_P < ASL_dN_lt_dS_P) {
+        ASL_dNdS_P <- 2 * ASL_dN_gt_dS_P
+      } else if(! is.na(ASL_dN_gt_dS_P) && ! is.na(ASL_dN_lt_dS_P)) {
+        ASL_dNdS_P <- 2 * ASL_dN_lt_dS_P
+      }
+      
+      if(ASL_dNdS_P == 0) {
+        ASL_dNdS_P <- 1 / NBOOTSTRAPS
+      }
+      
+      # Add to table
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_start <- lowest_codon_num
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_center <- (lowest_codon_num + highest_codon_num) / 2
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_end <- highest_codon_num
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_num_replicates <- num_replicates
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_N_diffs <- sum(unname(unlist(window_codon_data[ , paste0(NUMERATOR, '_diffs')])), na.rm = TRUE)
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_N_sites <- sum(unname(unlist(window_codon_data[ , paste0(DENOMINATOR, '_diffs')])), na.rm = TRUE)
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_S_diffs <- sum(unname(unlist(window_codon_data[ , paste0(NUMERATOR, '_sites')])), na.rm = TRUE)
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_S_sites <- sum(unname(unlist(window_codon_data[ , paste0(DENOMINATOR, '_sites')])), na.rm = TRUE)
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dN <- dN
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dS <- dS
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dNdS <- dNdS
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dN_m_dS <- dN_m_dS
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_SE <- boot_dN_SE
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dS_SE <- boot_dS_SE
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_over_dS_SE <- boot_dN_over_dS_SE
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_over_dS_P <- boot_dN_over_dS_P
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_m_dS_SE <- boot_dN_m_dS_SE
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_m_dS_P <- boot_dN_m_dS_P
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_gt_dS_count <- boot_dN_gt_dS_count
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_eq_dS_count <- boot_dN_eq_dS_count
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_lt_dS_count <- boot_dN_lt_dS_count
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dN_gt_dS_P <- ASL_dN_gt_dS_P
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dN_lt_dS_P <- ASL_dN_lt_dS_P
+      codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dNdS_P <- ASL_dNdS_P
+      
+    } # else leave it as NA
+  } # end last window
+} else if(nrow(codon_data) < WINDOW_SIZE) {
+  cat("\n\n### WARNING: the number of codons with num_defined_seqs>", MIN_DEFINED_CODONS , " is less than the WINDOW_SIZE=", WINDOW_SIZE, ".\n", sep="")
+  cat("### Thus, a sliding window using these parameters is not possible.\n")
+  cat("### TERMINATED.\n\n")
+  quit(save = 'no', status = 1, runLast = TRUE)
   
-  # Extract window; analyze
-  window_codon_data <- codon_data_filtered[i:(i + WINDOW_SIZE - 1), ]
-  lowest_codon_num <- min(window_codon_data$codon_num)
-  highest_codon_num <- max(window_codon_data$codon_num)
-  
-  if(nrow(window_codon_data) > 1) {
-    ### BOOTSTRAP EACH RATIO FOR THIS WINDOW. Columns:
-    #[1] num_replicates
-    #[2] dN
-    #[3] dS
-    #[4] dNdS
-    #[5] dN_m_dS
-    #[6] boot_dN_SE
-    #[7] boot_dS_SE
-    #[8] boot_dN_over_dS_SE
-    #[9] boot_dN_over_dS_P
-    #[10] boot_dN_m_dS_SE
-    #[11] boot_dN_m_dS_P
-    #[12] boot_dN_gt_dS_count
-    #[13] boot_dN_eq_dS_count
-    #[14] boot_dN_lt_dS_count
-    #[15] ASL_dN_gt_dS_P
-    #[16] ASL_dN_lt_dS_P
-    
-    # dN/dS bootstrap with appropriate correction
-    boot_dNdS <- NA
-    if(CORRECTION == "JC") {
-      boot_dNdS <- dNdS_diff_boot_fun_JC(window_codon_data, NUMERATOR, DENOMINATOR, NBOOTSTRAPS, NCPUS)
-    } else {
-      boot_dNdS <- dNdS_diff_boot_fun(window_codon_data, NUMERATOR, DENOMINATOR, NBOOTSTRAPS, NCPUS)
-    }
-    boot_dNdS <- str_split(string = boot_dNdS, pattern = '\t')[[1]]
-    
-    # bootstrap results
-    num_replicates <- as.integer(boot_dNdS[1])
-    dN <- as.numeric(boot_dNdS[2])
-    dS <- as.numeric(boot_dNdS[3])
-    dNdS <- as.numeric(boot_dNdS[4])
-    dN_m_dS <- as.numeric(boot_dNdS[5])
-    suppressWarnings(boot_dN_SE <- as.numeric(boot_dNdS[6]))
-    suppressWarnings(boot_dS_SE <- as.numeric(boot_dNdS[7]))
-    suppressWarnings(boot_dN_over_dS_SE <- as.numeric(boot_dNdS[8]))
-    suppressWarnings(boot_dN_over_dS_P <- as.numeric(boot_dNdS[9]))
-    suppressWarnings(boot_dN_m_dS_SE <- as.numeric(boot_dNdS[10]))
-    suppressWarnings(boot_dN_m_dS_P <- as.numeric(boot_dNdS[11]))
-    suppressWarnings(boot_dN_gt_dS_count <- as.integer(boot_dNdS[12]))
-    suppressWarnings(boot_dN_eq_dS_count <- as.integer(boot_dNdS[13]))
-    suppressWarnings(boot_dN_lt_dS_count <- as.integer(boot_dNdS[14]))
-    suppressWarnings(ASL_dN_gt_dS_P <- as.numeric(boot_dNdS[15]))
-    suppressWarnings(ASL_dN_lt_dS_P <- as.numeric(boot_dNdS[16]))
-    ASL_dNdS_P <- 1
-    
-    if(! is.na(ASL_dN_gt_dS_P) && ! is.na(ASL_dN_lt_dS_P) && ASL_dN_gt_dS_P < ASL_dN_lt_dS_P) {
-      ASL_dNdS_P <- 2 * ASL_dN_gt_dS_P
-    } else if(! is.na(ASL_dN_gt_dS_P) && ! is.na(ASL_dN_lt_dS_P)) {
-      ASL_dNdS_P <- 2 * ASL_dN_lt_dS_P
-    }
-    
-    if(ASL_dNdS_P == 0) {
-      ASL_dNdS_P <- 1 / NBOOTSTRAPS
-    }
-    
-    # Add to table
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_start <- lowest_codon_num
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_center <- (lowest_codon_num + highest_codon_num) / 2
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_end <- highest_codon_num
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_num_replicates <- num_replicates
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_N_diffs <- sum(unname(unlist(window_codon_data[ , paste0(NUMERATOR, '_diffs')])), na.rm = TRUE)
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_N_sites <- sum(unname(unlist(window_codon_data[ , paste0(DENOMINATOR, '_diffs')])), na.rm = TRUE)
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_S_diffs <- sum(unname(unlist(window_codon_data[ , paste0(NUMERATOR, '_sites')])), na.rm = TRUE)
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_S_sites <- sum(unname(unlist(window_codon_data[ , paste0(DENOMINATOR, '_sites')])), na.rm = TRUE)
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dN <- dN
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dS <- dS
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dNdS <- dNdS
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_dN_m_dS <- dN_m_dS
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_SE <- boot_dN_SE
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dS_SE <- boot_dS_SE
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_over_dS_SE <- boot_dN_over_dS_SE
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_over_dS_P <- boot_dN_over_dS_P
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_m_dS_SE <- boot_dN_m_dS_SE
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_m_dS_P <- boot_dN_m_dS_P
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_gt_dS_count <- boot_dN_gt_dS_count
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_eq_dS_count <- boot_dN_eq_dS_count
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_boot_dN_lt_dS_count <- boot_dN_lt_dS_count
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dN_gt_dS_P <- ASL_dN_gt_dS_P
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dN_lt_dS_P <- ASL_dN_lt_dS_P
-    codon_data[codon_data$codon_num == lowest_codon_num, ]$sw_ASL_dNdS_P <- ASL_dNdS_P
-    
-  } # else leave it as NA
-} # end last window
-
+} # end if at least WINDOW_SIZE
 
 ### SAVE RESULTS
 OUTFILE <- CODON_RESULTS_FILE
